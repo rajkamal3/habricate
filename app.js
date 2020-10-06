aa2fe1ab4d8c239ef5a72f9dc457cdf33e396d44
rajkamal3 <rkchenumalla@gmail.com>
2020-10-06 15:18:13 +0530

    nbd and basic error handling added

--- app.js
+++ app.js
@@ -6,14 +6,27 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
-    console.log('Hello from the middleware!');
-    next();
-});
-
 app.use(morgan('dev'));
 
 app.use('/api/v1/habits/', habitRouter);
 app.use('/api/v1/users/', userRouter);
+
+app.all('*', (req, res, next) => {
+    const err = new Error(`Cen't find ${req.originalUrl} on this server!`);
+    err.status = 'fail';
+    err.statusCode = 404;
+
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 500;
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+});
 
 module.exports = app;
