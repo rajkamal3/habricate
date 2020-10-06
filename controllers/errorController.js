6623a48b88e77dcc7e222dec1b0dd88733cae2a5
rajkamal3 <rkchenumalla@gmail.com>
2020-10-07 00:07:23 +0530

    unhandled rejections handled

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,7 @@
 const AppError = require('./../utils/appError');
 
 const handleCastErrorDB = err => {
-    const message = `Invalid ${err.path}: ${err.value}`;
+    const message = `Habit with the same name already exists!`;
     return new AppError(message, 400);
 };
 
@@ -38,7 +38,9 @@ module.exports = (err, req, res, next) => {
     } else if (process.env.NODE_ENV === 'production') {
         let error = { ...err };
 
-        if (error.name === 'CastError') error = handleCastErrorDB(error);
+        if (error.code === 11000) {
+            error = handleCastErrorDB(error);
+        }
 
         sendErrorProd(error, res);
     }
--- server.js
+++ server.js
@@ -19,6 +19,15 @@ mongoose
     .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('Unhandled rejection! Shutting down...');
+    server.close(() => {
+        process.exit(1);
+    });
 });
