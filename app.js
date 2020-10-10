a236b74b0c50319e0f862a7b54610e9875cc47c4
rajkamal3 <rkchenumalla@gmail.com>
2020-10-10 20:29:58 +0530

    parameter pollution prevented with hpp

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const app = express();
 
@@ -28,6 +29,12 @@ app.use('/api', limiter);
 app.use(mongoSanitize());
 
 app.use(xss());
+
+app.use(
+    hpp({
+        whitelist: ['goal']
+    })
+);
 
 app.use('/api/v1/habits/', habitRouter);
 app.use('/api/v1/users/', userRouter);
