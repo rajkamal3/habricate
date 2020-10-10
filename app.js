fe83cab74e2a8f3bcfca1ce82fc11b1d92019908
rajkamal3 <rkchenumalla@gmail.com>
2020-10-10 20:24:14 +0530

    data sanitization middlewares added

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const rateLimit = require('express-rate-limit');
-const hemlet = require('helmet');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const app = express();
 
@@ -22,6 +24,10 @@ const limiter = rateLimit({
 });
 
 app.use('/api', limiter);
+
+app.use(mongoSanitize());
+
+app.use(xss());
 
 app.use('/api/v1/habits/', habitRouter);
 app.use('/api/v1/users/', userRouter);
