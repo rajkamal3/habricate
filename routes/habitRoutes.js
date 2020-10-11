33c473cd77ff1584bc6723b5703a6da4e686eb6f
rajkamal3 <rkchenumalla@gmail.com>
2020-10-11 14:48:38 +0530

    nested routes implemented

--- routes/habitRoutes.js
+++ routes/habitRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const habitController = require('./../controllers/habitController');
 const authController = require('./../controllers/authController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
+
+router.use('/:habitId/reviews', reviewRouter);
 
 router
     .route('/top-5-habits')
