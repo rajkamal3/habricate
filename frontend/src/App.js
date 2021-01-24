14bd8dd02609d32bbbe5cb6f4443ceffd9b260c4
rajkamal3 <rkchenumalla@gmail.com>
2021-01-24 19:46:06 +0530

    habit card template implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css';
+import HomeScreen from './screens/homeScreen/homeScreen';
 
 function App() {
-    const [huell, setHuell] = useState('');
+    const [huell, setHuell] = useState([]);
 
     useEffect(() => {
         axios.get(`http://127.0.0.1:3000/api/v1/habits/`).then(res => {
-            console.log(res);
+            const { data } = res.data.data;
+            setHuell(data);
         });
     }, []);
 
-    return <div className="App">{huell}</div>;
+    return (
+        <div>
+            <HomeScreen />
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/screens/homeScreen/homeScreen.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import CurrentHabits from './../../components/currentHabits/currentHabits';
+import user from './../../assets/images/user.png';
+
+const homeScreen = () => {
+    return (
+        <div>
+            <header
+                style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    height: '70px',
+                    width: '100%',
+                    backgroundColor: '#fff',
+                    alignItems: 'center',
+                    boxShadow: '0px -25px 40px 0px rgba(0,0,0,0.5)'
+                }}
+            >
+                <div
+                    style={{
+                        height: 'inherit',
+                        width: '220px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        fontWeight: '900',
+                        fontSize: '34px',
+                        color: '#66a851'
+                    }}
+                >
+                    Habricate
+                </div>
+                <div
+                    style={{
+                        height: '70px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        width: '100px',
+                        justifyContent: 'center',
+                        cursor: 'pointer'
+                    }}
+                >
+                    <img
+                        src={user}
+                        style={{
+                            height: '50px'
+                        }}
+                        alt="user"
+                    />
+                </div>
+            </header>
+            <CurrentHabits />
+        </div>
+    );
+};
+
+export default homeScreen;
