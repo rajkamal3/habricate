09d0365634e5214372017689d8e0f61ccb6ebab8
rajkamal3 <rkchenumalla@gmail.com>
2021-01-28 13:44:35 +0530

    login implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -19,3 +19,21 @@ exports.signup = async (req, res, next) => {
         });
     }
 };
+
+exports.login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        res.status(201).json({
+            status: 'success',
+            result: user
+        });
+    } else {
+        res.status(400).json({
+            status: 'fail',
+            res: res.err
+        });
+    }
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 
+const { signup, login } = userController;
+
 const router = express.Router();
 
-router.route('/signup').post(userController.signup);
+router.route('/signup').post(signup);
+router.route('/login').post(login);
 
 module.exports = router;
