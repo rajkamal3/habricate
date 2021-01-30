2e9be979cf8e5fd8c8954580f0f0350e748f92ef
rajkamal3 <rkchenumalla@gmail.com>
2021-01-30 23:41:07 +0530

    getMyHabits controller and route implemented

--- backend/controllers/habitController.js
+++ backend/controllers/habitController.js
@@ -1,15 +1,6 @@
 const Habit = require('./../models/habitModel');
 const catchAsync = require('express-async-handler');
 
-exports.getAllHabits = catchAsync(async (req, res, next) => {
-    const habits = await Habit.find({});
-
-    res.status(200).json({
-        status: 'success',
-        data: habits
-    });
-});
-
 exports.createHabit = async (req, res, next) => {
     const habit = await Habit.create(req.body);
 
@@ -18,3 +9,21 @@ exports.createHabit = async (req, res, next) => {
         data: habit
     });
 };
+
+exports.getMyHabits = async (req, res, next) => {
+    const habits = await Habit.find({ user: req.user._id });
+
+    res.status(201).json({
+        status: 'success',
+        data: habits
+    });
+};
+
+exports.getAllHabits = catchAsync(async (req, res, next) => {
+    const habits = await Habit.find({});
+
+    res.status(200).json({
+        status: 'success',
+        data: habits
+    });
+});
--- backend/routes/habitRoutes.js
+++ backend/routes/habitRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const habitController = require('./../controllers/habitController');
 const { protect } = require('./../controllers/userController');
 
-const { getAllHabits, createHabit } = habitController;
+const { createHabit, getMyHabits, getAllHabits } = habitController;
 
 const router = express.Router();
 
 router.route('/').get(protect, getAllHabits).post(createHabit);
+router.route('/myHabits').get(protect, getMyHabits).post(createHabit);
 
 module.exports = router;
