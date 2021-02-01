fd91b84d23d22ad9b439ed2cffacd276d2fc2da2
rajkamal3 <rkchenumalla@gmail.com>
2021-02-02 00:37:32 +0530

    loading spinner added for home screen

--- frontend/src/screens/homeScreen/homeScreen.js
+++ frontend/src/screens/homeScreen/homeScreen.js
@@ -1,112 +1,39 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchAllHabitsOfUser } from './../../actions/habitActions';
+import styles from './homeScreen.module.css';
 import book from './../../assets/images/book.png';
+import Spinner from './../../ui/spinner/spinner';
 
 const HomeScreen = () => {
     const dispatch = useDispatch();
 
-    const habitsStore = useSelector(state => state.habits);
-    const { loading, habits } = habitsStore;
+    const userHabitsStore = useSelector(state => state.userHabits);
+    const { loading, habits } = userHabitsStore;
 
     useEffect(() => {
         dispatch(fetchAllHabitsOfUser());
     }, [dispatch]);
 
     return (
-        <div
-            style={{
-                backgroundColor: 'red',
-                width: '100%',
-                height: 'calc(100vh - 70px)',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                color: '#222'
-            }}
-        >
-            <div
-                style={{
-                    width: '90%',
-                    height: '93%',
-                    backgroundColor: 'green'
-                }}
-            >
-                <div
-                    style={{
-                        fontWeight: '700'
-                    }}
-                >
-                    Current habits
-                </div>
-                {loading && <div>Loading...</div>}
+        <div className={styles.homeScreenContainer}>
+            <div className={styles.homeScreenContainerChild}>
+                <div className={styles.currentHabitsTitle}>Current habits</div>
+                {loading && <Spinner />}
                 {habits &&
                     habits.map(habit => {
                         return (
-                            <div
-                                style={{
-                                    width: '100%',
-                                    backgroundColor: 'mediumvioletred',
-                                    height: '150px',
-                                    borderRadius: '10px',
-                                    marginTop: '10px',
-                                    boxShadow: '0px 0px 10px -3px rgba(0,0,0,0.5)',
-                                    display: 'flex',
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                    flexDirection: 'column'
-                                }}
-                                key={habit._id}
-                            >
-                                <div
-                                    style={{
-                                        backgroundColor: 'orange',
-                                        width: '90%',
-                                        height: '75%',
-                                        display: 'flex',
-                                        flexDirection: 'row'
-                                    }}
-                                >
-                                    <div
-                                        style={{
-                                            backgroundColor: '#edeeef',
-                                            width: '25%',
-                                            borderRadius: '10px',
-                                            display: 'flex',
-                                            justifyContent: 'center',
-                                            alignItems: 'center'
-                                        }}
-                                    >
+                            <div className={styles.eachHabit} key={habit._id}>
+                                <div className={styles.eachHabitChild}>
+                                    <div className={styles.habitImageContainer}>
                                         <img src={book} width="45px" alt="book" />
                                     </div>
-                                    <div
-                                        style={{
-                                            backgroundColor: '#81d4fa',
-                                            width: '70%',
-                                            boxSizing: 'border-box',
-                                            display: 'flex',
-                                            flexDirection: 'column',
-                                            justifyContent: 'space-around',
-                                            fontSize: '20px',
-                                            fontWeight: '500'
-                                        }}
-                                    >
+                                    <div className={styles.habitDetailsContainer}>
                                         <div>{habit.name}</div>
-                                        <div>{habit.doAt}</div>
-                                        <div>{habit.goalUnits}</div>
+                                        <div>{habit.doAtTime.join(' ')}</div>
+                                        <div>{habit.dailyTargetUnit}</div>
                                     </div>
-                                    <div
-                                        style={{
-                                            width: '5%',
-                                            display: 'flex',
-                                            justifyContent: 'center',
-                                            alignItems: 'center',
-                                            fontWeight: '400',
-                                            transform: 'scaleY(2)'
-                                        }}
-                                    >
-                                        &#x3e;
-                                    </div>
+                                    <div className={styles.eachHabitRightArrow}>&#x3e;</div>
                                 </div>
                             </div>
                         );
