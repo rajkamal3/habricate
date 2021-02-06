dbb0ba142f2cf5a5056ab6e7498e215b781902e6
rajkamal3 <rkchenumalla@gmail.com>
2021-02-06 19:52:32 +0530

    interim commit

--- frontend/src/ui/addHabit/addHabit.js
+++ frontend/src/ui/addHabit/addHabit.js
@@ -1,3 +1,4 @@
+import { sign } from 'jsonwebtoken';
 import React from 'react';
 
 const AddHabit = () => {
@@ -7,13 +8,45 @@ const AddHabit = () => {
                 width: '85%',
                 height: '30%',
                 position: 'absolute',
-                backgroundColor: 'red',
-                opacity: '0.3',
-                marginTop: '20px',
-                zIndex: 999
+                marginTop: '34px',
+                zIndex: '999',
+                backgroundColor: 'white',
+                borderRadius: '10px',
+                boxShadow: 'rgb(0 0 0 / 25%) 0px 0px 20px -3px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column'
             }}
         >
-            Add habit huelelerer
+            <div
+                style={{
+                    width: '80%',
+                    height: '65%',
+                    // backgroundColor: 'red',
+                    fontSize: '17px',
+                    fontWeight: 500,
+                    lineHeight: '24px',
+                    color: '#888888'
+                }}
+            >
+                <div>
+                    At{' '}
+                    <input
+                        type="time"
+                        style={{
+                            width: '74px',
+                            border: 'none',
+                            borderBottom: '2px solid rgb(102, 168, 81)',
+                            fontFamily: "'Gilroy'",
+                            fontWeight: '500',
+                            fontSize: '17px',
+                            color: 'rgb(102, 168, 81)'
+                        }}
+                    />{' '}
+                    everyday, I want to read a book on the study table.
+                </div>
+            </div>
         </div>
     );
 };
