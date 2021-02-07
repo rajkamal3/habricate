14b4dc2acbf5b4d980faaf7497c82585ce050380
rajkamal3 <rkchenumalla@gmail.com>
2021-02-07 23:28:05 +0530

    interim commit

--- frontend/src/ui/addHabit/addHabit.js
+++ frontend/src/ui/addHabit/addHabit.js
@@ -9,9 +9,24 @@ const AddHabit = () => {
                 <div>
                     At <input type="time" className={styles.addHabitDateInput} /> <img src={plus} alt="plus" width="22px" />
                     <span>
-                        everyday, I want to <input type="text" className={styles.addHabitTextInputs} placeholder="read a book" /> at the
+                        everyday, I want to <input type="text" className={styles.addHabitTextInputs} placeholder="read a book" /> at the{' '}
                         <input type="text" className={styles.addHabitTextInputs} placeholder="study table" />.
                     </span>
+                </div>
+                <div>
+                    <input type="checkbox" id="dailyGoal" />
+                    <label htmlFor="dailyGoal">
+                        Daily goal
+                        <input type="text" className={styles.addHabitDateInput} />
+                        <input type="text" className={styles.addHabitDateInput} />
+                    </label>
+                </div>
+                <div>
+                    <input type="checkbox" id="reminder" />
+                    <label htmlFor="reminder">Reminder</label>
+                </div>
+                <div className={styles.addHabitButtonContainer}>
+                    <div className={styles.addHabitButton}>Add habit</div>
                 </div>
             </div>
         </div>
