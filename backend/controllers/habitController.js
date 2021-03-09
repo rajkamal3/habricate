d3c5f4328667bf29409eed148b50199d51902514
rajkamal3 <rkchenumalla@gmail.com>
2021-03-09 23:21:36 +0530

    interim commit - date changes

--- backend/controllers/habitController.js
+++ backend/controllers/habitController.js
@@ -31,7 +31,7 @@ exports.getAllHabits = catchAsync(async (req, res, next) => {
 exports.getSingleHabit = catchAsync(async (req, res, next) => {
     const habit = await Habit.findById(req.params.id);
 
-    console.log(Date.now());
+    console.log(Date());
 
     res.status(201).json({
         status: 'success',
